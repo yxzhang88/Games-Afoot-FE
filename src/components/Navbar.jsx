8089e01130c00203c5e8fb1f3df889e3275c315e
Miranda <yxzhang135@gmail.com>
2024-08-05 13:13:24 -0700

    Added Navbar.css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-// import "./navbar.css";
+import "./Navbar.css";
 import { useState, useRef, useEffect } from "react";
 import logo1 from "../assets/logo1.png";
 
