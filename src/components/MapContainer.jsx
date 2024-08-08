0338c7f91f4f767a036b23e10b90c000370cdf0b
Miranda <yxzhang135@gmail.com>
2024-08-08 02:27:23 -0700

    Added dynamic color marker and animation affecting position

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -3,11 +3,11 @@ import {
     TileLayer,
     useMap,
 } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; // Import Leaflet CSS for default styling
+import "leaflet/dist/leaflet.css"; 
 import "./MapStyles.css";
-import PropTypes from "prop-types"; // Import PropTypes for prop validation
-import LocationMarker from "./LocationMarker"; // LocationMarker component
-import L from "leaflet"; // Leaflet for default icon fixes
+import PropTypes from "prop-types"; 
+import LocationMarker from "./LocationMarker"; 
+import L from "leaflet"; 
 import { useState, useEffect } from "react";
 import InstructionPopUp from "./InstructionPopUp";
 
@@ -20,25 +20,23 @@ L.Icon.Default.mergeOptions({
     shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
-// Custom hook to center map on position change
 const MapCenter = ({ position }) => {
-    const map = useMap(); // Access the Leaflet map instance
+    const map = useMap(); 
 
     useEffect(() => {
         if (position) {
-            map.setView(position, map.getZoom()); // Update map center to the new position
+            map.setView(position, map.getZoom()); 
         }
-    }, [position, map]); // Depend on position and map
+    }, [position, map]); 
 
     return null;
 };
 
 MapCenter.propTypes = {
-    position: PropTypes.arrayOf(PropTypes.number).isRequired, // Validate that position is an array of numbers
+    position: PropTypes.arrayOf(PropTypes.number).isRequired, 
 };
 
 const MapContainer = ({ updateLocation }) => {
-    // State for map center position with default coordinates
     const [position, setPosition] = useState([47.636719, -122.366806]);
     const [initialLoad, setInitialLoad] = useState(true);
 
@@ -47,19 +45,18 @@ const MapContainer = ({ updateLocation }) => {
             const getLocation = () => {
                 navigator.geolocation.getCurrentPosition(
                     (pos) => {
-                        const { latitude, longitude } = pos.coords; // Extract latitude and longitude from position object
+                        const { latitude, longitude } = pos.coords; 
                         const newPosition = [latitude, longitude];
-                        setPosition(newPosition); // Update position state with new coordinates
-                        updateLocation(newPosition); // Pass new coordinates to parent component
+                        setPosition(newPosition); 
+                        updateLocation(newPosition); 
                     },
                     (error) => {
-                        console.error("Error retrieving position:", error); // Error handling
+                        console.error("Error retrieving position:", error); 
                     },
-                    { enableHighAccuracy: true } // Use high accuracy if possible
+                    { enableHighAccuracy: true } 
                 );
             };
 
-            // Get location on initial load
             if (initialLoad) {
                 getLocation();
                 setInitialLoad(false);
@@ -67,7 +64,7 @@ const MapContainer = ({ updateLocation }) => {
         } else {
             console.error("Geolocation not supported by this browser.");
         }
-    }, [updateLocation, initialLoad]); // Dependency on updateLocation and initialLoad
+    }, [updateLocation, initialLoad]); 
 
     return (
         <LeafletMapContainer
@@ -89,7 +86,7 @@ const MapContainer = ({ updateLocation }) => {
 };
 
 MapContainer.propTypes = {
-    updateLocation: PropTypes.func.isRequired, // Validate that updateLocation is a function
+    updateLocation: PropTypes.func.isRequired, 
 };
 
 export default MapContainer;
